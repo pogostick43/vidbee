0a41dec09b5aff6802b9732d14dfd32d7b907be3
pogostick43 <38359306+pogostick43@users.noreply.github.com>
2023-04-05 10:46:54 +0200

    Update setup-inputs.js

--- setup-inputs.js
+++ setup-inputs.js
@@ -1,163 +1,43 @@
-function setTextAreaText(assetDesc, isFullVideo){
-  var text = assetDesc.querySelectorAll('[name^="brief-"]')[0].value;
-  var textUnchanged = text==fullVideoText || text==sceneText || text=="";
-  if(textUnchanged && isFullVideo){
-  	assetDesc.querySelectorAll('[name^="brief-"]')[0].value = fullVideoText;
-  }else if(textUnchanged && !isFullVideo){
-    assetDesc.querySelectorAll('[name^="brief-"]')[0].value = sceneText;
-  }
+
+//Set up for every asset. Hide lang box
+function setupInputs(sceneContainer){
+	const langCheckbox = $(sceneContainer).find('input[name="uses_lang"]')[0];
+	const langField = sceneContainer.getElementsByClassName('lang-selector')[0];
+	const langInputs = $(langField).find('input[type="radio"]');
+	console.log("set list");
+	// Add a change event listener to the checkbox
+	langCheckbox.addEventListener('change', function()
+	{
+	    //clean the inputs
+	    langInputs.each(function()
+	    {
+	        $(this).prop('checked', false);
+	        $(this).parent().removeClass('highlight');
+	    });
+	    // If the checkbox is checked, hide the age range field div; otherwise, show it
+	    if (langCheckbox.checked)
+	    {
+	        langField.style.display = 'flex';
+	    }
+	    else
+	    {
+	        langField.style.display = 'none';
+	    }
+	});
 }
 
-function setSummary(sceneContainer){
-    
-    const fullVideoContainer = sceneContainer.getElementsByClassName("full-video-spec")[0];
-    const extraAssetContainer = sceneContainer.getElementsByClassName("extra-scene-spec")[0];
-    const assetDesc = sceneContainer.getElementsByClassName("brief-spec")[0];
-
-    //set initialized state invisible
-    fullVideoContainer.style.display = "none";
-    extraAssetContainer.style.display = "none";
-    assetDesc.style.display = "none";
-
-    const summaryType = sceneContainer.querySelector('.summary-text');
-    const summaryCategory = sceneContainer.querySelector('.summary-category');
-    const summaryFormat = sceneContainer.querySelector('.summary-format');
-    const summaryLength = sceneContainer.querySelector('.summary-length');
-    let isFullVideo = false;
-    let selectorOnes = sceneContainer.getElementsByClassName("selector-one");
-    for (let sc_i = 0; sc_i < selectorOnes.length; sc_i++)
-    {
-        let selectorLabels = selectorOnes[sc_i].getElementsByTagName('label');
-        for (let i_i = 0; i_i < selectorLabels.length; i_i++)
-        {
-            let selectorInput = selectorLabels[i_i].getElementsByTagName("input")[0];
-            let displayText = selectorLabels[i_i].getElementsByTagName("span")[0].innerHTML;
-            //TYPE
-            if (selectorInput.name.startsWith('type'))
-            {
-                console.log("set eventlist");
-                selectorInput.addEventListener("click", function()
-                {
-                    summaryType.innerHTML = displayText;
-                    //set visible
-                    if (selectorInput.value === 'Full-Video')
-                    {
-                        extraAssetContainer.style.display = "none";
-                        fullVideoContainer.style.display = "block";
-                        assetDesc.style.display = "block";
-                        isFullVideo = true;
-                        setTextAreaText(assetDesc, isFullVideo);
-                    }
-                    else if (selectorInput.value === 'Scene')
-                    {
-                        fullVideoContainer.style.display = "none";
-                        extraAssetContainer.style.display = "block";
-                        assetDesc.style.display = "block";                        
-                        isFullVideo = false;
-                        setTextAreaText(assetDesc, isFullVideo);
-                        summaryFormat.innerHTML = "";
-                        summaryLength.innerHTML = "";
-                    }
-                })
-            }
-            //CATEGORY
-            else if (selectorInput.name.startsWith('category'))
-            {
-                selectorInput.addEventListener("click", function()
-                {
-                    summaryCategory.innerHTML = displayText;
-                })
-            }
-            //Format
-            else if (selectorInput.name.startsWith('delivery'))
-            {
-                selectorInput.addEventListener("click", function()
-                {
-                    if (isFullVideo)
-                    {
-                        summaryFormat.innerHTML = displayText;
-                    }
-                })
-            }
-            //length
-            else if (selectorInput.name.startsWith('length'))
-            {
-                selectorInput.addEventListener("click", function()
-                {
-                    if (isFullVideo)
-                    {
-                        summaryLength.innerHTML = displayText;
-                    }
-                })
-            }
-        }
+//Set up age range once. Hide it.
+const allAgesCheckbox = document.getElementById('All-ages-accepted');
+// Get the age range field div with the id "age-range-field"
+const ageRangeField = document.getElementById('age-range-field');
+// Add a change event listener to the checkbox
+allAgesCheckbox.addEventListener('change', function()
+{
+    // If the checkbox is checked, hide the age range field div; otherwise, show it
+    if (allAgesCheckbox.checked){
+        ageRangeField.style.display = 'none';
     }
-}
-
-//Set the actions for the asset summary buttons
-function setSummaryActions(sceneContainer){
-    const summarySection = sceneContainer.querySelector('.summary');
-    summarySection.style.display = "none";
-    //SAVE - MINIMIZE
-    const saveBtn = sceneContainer.querySelector('.save-btn');
-    const newAssetSecion = sceneContainer.querySelector('.new-asset-section');
-    saveBtn.addEventListener("click", function()
-    {
-        newAssetSecion.style.display = "none";
-        summarySection.style.display = "flex";
-
-        //Send data to wized. This will also display and clear any errors.
-        Wized.data.setVariable("assetdata", getAssetData()[0]);
-
-    });
-    //EDIT - ENLARGE
-    const editBtn = sceneContainer.querySelector('.edit-btn');
-    editBtn.addEventListener("click", function()
-    {
-        newAssetSecion.style.display = "block";
-        summarySection.style.display = "none";
-    });
-    //DELETE SUMMARY
-    const delBtn_summary = sceneContainer.querySelector('.trash-summary');
-    delBtn_summary.addEventListener("click", function()
-    {
-        sceneContainer.remove();
-        updatePrice();
-    });
-    //DELETE ASSET PAGE
-    const delBtn_asset = sceneContainer.querySelector('.trash-asset');
-    delBtn_asset.addEventListener("click", function()
-    {
-        sceneContainer.remove();
-        updatePrice();
-    });
-    //Remove all checked, do not include type
-    const checkedBoxes = sceneContainer.getElementsByClassName("row");
-    for (let h_i = 1; h_i < checkedBoxes.length; h_i++)
-    {
-        let input = checkedBoxes[h_i].getElementsByTagName("input")[0];
-        if (!input.name.startsWith('type'))
-        {
-            input.checked = false;
-        }
+    else{
+        ageRangeField.style.display = 'flex';
     }
-}
-
-//Set and display the price for a single asset
-function setAssetPrice(sceneContainer){
-    $('input[type="radio"][price]').on('change', function()
-    {
-        //sum up all "price" in scene container where input is checked
-        var assetPrice = 0; // initialize total price to zero
-        $(sceneContainer).find('input[type="radio"][price]:checked').each(function()
-        {
-            var itemPrice = this.getAttribute('price');
-            if (itemPrice)
-            {
-                assetPrice += parseInt(itemPrice);
-            } // add price of checked input to total}
-        });
-        sceneContainer.querySelector('.price-summary').innerHTML = "€" + assetPrice;
-        updatePrice();
-    });
-}
+});
